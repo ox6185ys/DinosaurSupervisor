8646a4f7d9b1aaf27c10db9a09f25a3f7533b96b
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-02-18 18:11:28 -0600

    Working with GIT

---
+++ src/com/company/Dinosaur.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by ox6185ys on 2/18/2016.
+ */
+public class Dinosaur {
+    /**
+     * Created by ox6185ys on 2/18/2016.
+     */
+    public static class DinosaurSupervisor {
+                public static void main(String[] args){
+            Dinosaur t_rex = new Dinosaur();
+            Dinosaur velociraptor = new Dinosaur();
+
+            t_rex.roar();
+            velociraptor.roar();
+        }
+    }
+
+    private void roar() {
+    }
+}
---
+++ src/com/company/Supervisor.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ox6185ys on 2/18/2016.
+ */
+public class Supervisor {
+    public void roar (){
+        System.out.println("GRRRRR!!!!");
+    }
+}
